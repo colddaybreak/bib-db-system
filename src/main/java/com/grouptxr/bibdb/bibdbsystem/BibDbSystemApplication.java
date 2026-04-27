a3a78826b3db255904d4fd706d3aa4e004439fbc
colddaybreak <910829929@qq.com>
2026-04-27 15:00:24 +0800

    BASE update-2026/4/27

---
+++ src/main/java/com/grouptxr/bibdb/bibdbsystem/BibDbSystemApplication.java
@@ -0,0 +1,13 @@
+package com.grouptxr.bibdb.bibdbsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibDbSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BibDbSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/grouptxr/bibdb/bibdbsystem/BibDbSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grouptxr.bibdb.bibdbsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibDbSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
